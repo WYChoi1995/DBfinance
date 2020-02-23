966f84973325847411af949e61f087bd43666b41
WYChoi1995 <60026903+WYChoi1995@users.noreply.github.com>
2020-02-23 19:52:21 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import time
-from ticklog import *
-
-if __name__ == "__main__":
-    start_time = time.time()
-    binance = GetData()
-    key = binance.get_tick_data("ETHBTC")
-    key2 = binance.get_tick_data("BTCUSDT")
-    binance.socket.start()
